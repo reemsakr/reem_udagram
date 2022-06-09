2a8820882d74bcbfcb0e6306748138637ba5f4fc
reemsakr <reemsakr060@gmail.com>
2022-06-09 07:39:37 +0200

    deploying

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-back.eba-8qa5wkgr.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api.eba-8qa5wkgr.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-back.eba-8qa5wkgr.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api.eba-8qa5wkgr.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
